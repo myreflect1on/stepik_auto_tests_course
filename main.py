88237d1b4839fb5ef34c1e4358698cb7206c54bd
myreflect1on <z.denis.mail@gmail.com>
2021-12-15 13:58:02 +0500

    Initial commit

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
         print_hi('PyCharm')
         if __name__ == '__main__':
             print_hi('PyCharm')
-            if __name__ == '__main__':
-                print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
