5978e5a1bdb8258076f779fb9fa01a15d3793226
myreflect1on <z.denis.mail@gmail.com>
2021-12-15 16:13:33 +0500

    Initial commit

---
+++ 3.2.6.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+import selenium
+assert abs(-42) == -42, "Should be absolute value of a number"
+
